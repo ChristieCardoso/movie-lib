56e4d2641e61ef614cd898717e632cfdcc2d8890
ChristieCardoso <christiecardoso@hotmail.com>
2023-12-10 18:30:24 -0300

    edit movie

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -21,7 +21,6 @@ export const Movie = () => {
   const getMovie = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
     setMovie(data);
   };
 
